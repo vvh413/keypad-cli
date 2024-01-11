15d69fa56b3fb294f5e5988573b85db02aceb9da
vvh413 <nnenekit@gmail.com>
2024-01-12 00:11:43 +0300

    added media keys support

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
 use anyhow::Result;
-use usbd_hid::descriptor::KeyboardUsage;
+use usbd_hid::descriptor::{KeyboardUsage, MediaKey};
 
-const DEFAULT: [u16; 4] = [
-  KeyboardUsage::KeyboardF13 as u16,
-  KeyboardUsage::KeyboardF14 as u16,
-  KeyboardUsage::KeyboardF15 as u16,
-  KeyboardUsage::KeyboardF16 as u16,
+const DEFAULT: [u32; 4] = [
+  KeyboardUsage::KeyboardF13 as u32,
+  KeyboardUsage::KeyboardF14 as u32,
+  KeyboardUsage::KeyboardF15 as u32,
+  KeyboardUsage::KeyboardF16 as u32,
 ];
 
 fn main() -> Result<()> {
   let api = hidapi::HidApi::new()?;
   let device = api.open(0x7668, 0x0001)?;
 
-  let (modifiers, keys): (Vec<u8>, Vec<u8>) = std::env::args()
+  let (types, (modifiers, keys)): (Vec<u8>, (Vec<u8>, Vec<u8>)) = std::env::args()
     .skip(1)
     .take(4)
     .zip(DEFAULT)
     .map(|(arg, default)| {
-      let key = u16::from_str_radix(&arg, 16).unwrap_or(default);
-      ((key >> 8) as u8, (key & 0xff) as u8)
+      let [_, r#type, mods, key] = u32::from_str_radix(&arg, 16).unwrap_or(default).to_be_bytes();
+      (r#type, (mods, key))
     })
     .unzip();
   assert_eq!(modifiers.len(), 4, "invalid argument count");
 
-  device.write(&[keys.clone(), modifiers.clone()].concat())?;
+  device.write(&[types.clone(), keys.clone(), modifiers.clone()].concat())?;
 
   println!("flashed");
-  modifiers
+  types
     .iter()
+    .zip(modifiers)
     .zip(keys)
-    .for_each(|(modifier, key)| println!(" {modifier:08b} {key:02x} {:?}", KeyboardUsage::from(key)));
+    .for_each(|((r#type, modifier), key)| match r#type {
+      0 => println!(" {modifier:08b} {key:02x} {:?}", KeyboardUsage::from(key)),
+      1 => println!("    media {key:02x} {:?}", MediaKey::from(key)),
+      _ => unreachable!(),
+    });
 
   Ok(())
 }
